90f86d4d2e63d7c346c042df6ec8a464f0890777
Michael Omonedo <rukkyomonedo@gmail.com>
2025-03-31 15:17:05 +0100

    basic app js setup

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+import express from "express"
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Express.js server!")
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Subscription tracker API is running on localhost ${port}`);
+});
+
+export default app
