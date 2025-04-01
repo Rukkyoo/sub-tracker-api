bf010c0aa4d17f6d3c07e6993db5cecb1abe7092
Michael Omonedo <rukkyomonedo@gmail.com>
2025-04-01 16:52:05 +0100

    zzz

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from "express"
+import { PORT } from "./config/env.js"
 const app = express()
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Express.js server!")
 })
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Subscription tracker API is running on localhost ${port}`);
+
+app.listen(PORT, () => {
+    console.log(`Subscription tracker API is running on localhost ${PORT}`);
 });
 
 export default app
+
