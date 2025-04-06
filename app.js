039c5b467ddd298e1bc303de2802e0abda7feaf2
Michael Omonedo <rukkyomonedo@gmail.com>
2025-04-06 16:07:19 +0100

    completed signin logic

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 import express from "express"
 import { PORT } from "./config/env.js"
+import cookieParser from "cookie-parser"
 import userRouter from "./routes/user.routes.js"
 import authRouter from "./routes/auth.routes.js"
 import subscriptionsRouter from "./routes/subscription.routes.js"
 import connectToDatabase from "./Database/mongodb.js"
+import errorMiddleWare from "./middleware/error.middleware.js"
 const app = express()
 
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/subscriptions", subscriptionsRouter)
+
+app.use(errorMiddleWare)
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Express.js server!")
--- config/env.js
+++ config/env.js
@@ -1,6 +1,6 @@
-import { config } from 'dotenv';
-
+import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const { PORT, NODE_ENV, MONGO_URI } = process.env 
+export const { PORT, NODE_ENV, MONGO_URI, JWT_SECRET, JWT_EXPIRES_IN } =
+  process.env;
