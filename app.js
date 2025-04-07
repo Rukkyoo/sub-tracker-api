f7fe6a7aaaa0f03c9270021b8828e7d3737f6479
Michael Omonedo <rukkyomonedo@gmail.com>
2025-04-07 14:01:30 +0100

    using express rate limit

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
-import express from "express"
-import { PORT } from "./config/env.js"
-import cookieParser from "cookie-parser"
-import userRouter from "./routes/user.routes.js"
-import authRouter from "./routes/auth.routes.js"
-import subscriptionsRouter from "./routes/subscription.routes.js"
-import connectToDatabase from "./Database/mongodb.js"
-import errorMiddleWare from "./middleware/error.middleware.js"
-const app = express()
+import express from "express";
+import { PORT } from "./config/env.js";
+import cookieParser from "cookie-parser";
+import { rateLimit } from "express-rate-limit";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionsRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./Database/mongodb.js";
+import errorMiddleWare from "./middleware/error.middleware.js";
+const app = express();
 
-
-//  Middleware should be used before routes
-app.use(errorMiddleWare)
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-
-
-//  Routes should always come last
-app.use("/api/v1/auth", authRouter)
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/subscriptions", subscriptionsRouter)
-
-app.get("/", (req, res) => {
-    res.send("Welcome to the Express.js server!")
-})
-
-
-app.listen(PORT, async () => {
-    console.log(`Subscription tracker API is running on localhost ${PORT}`);
-    await connectToDatabase()
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
 });
 
-export default app
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
 
+//  Middleware should be used before routes
+app.use(errorMiddleWare);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//  Routes should always come last
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionsRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Express.js server!");
+});
+
+app.listen(PORT, async () => {
+  console.log(`Subscription tracker API is running on localhost ${PORT}`);
+  await connectToDatabase();
+});
+
+export default app;
